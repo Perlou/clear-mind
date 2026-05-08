a3808a655a99208e9a99a6b4cbbb33962f61f859
Perlou <perloukevin@gmail.com>
2026-05-08 13:08:57 +0800

    fix: layout

--- space/app.py
+++ space/app.py
@@ -97,8 +97,8 @@ st.markdown(
         .stApp > div:last-child {
             margin-bottom: -35px !important;
         }
-        .stApp {
-            margin-bottom: 90px !important;
+        .stBottom {
+            bottom: 100px !important;
         }
     </style>
 """,
